991f92629b8bf425b9daac5fc1e112dd1419811d
Robert Horn <hornrj@robs.office>
2018-02-27 14:12:16 -0500

    Change drawer names, UID name.  Move location from drawer to headline.

--- ical2org.go
+++ ical2org.go
@@ -47,8 +47,8 @@ func main() {
 				// print the event
 				fmt.Printf("* %s <%s>\n", strings.Replace(event[0].GetSummary(), `\,`, ",", -1), event[0].GetStart().Format("2006-01-02 15:04"))
 				fmt.Printf("    SCHEDULED: <%s-%s>\n", event[0].GetStart().Format("2006-01-02 15:04"), event[0].GetEnd().Format("15:04"))
-				fmt.Println("  :PROPERTIES:")
-				fmt.Printf("  :UID: %s\n", event[0].GetID())
+				fmt.Println("  :ICALCONTENTS:")
+				fmt.Printf("  :ORGUID: %s\n", event[0].GetID())
 				fmt.Printf("  :ORIGINAL-UID: %s\n", event[0].GetImportedID())
 				fmt.Printf("  :DTSTART: %s\n", event[0].GetStart().Format("2006-01-02 15:04"))
 				fmt.Printf("  :DTEND: %s\n", event[0].GetEnd().Format("2006-01-02 15:04"))
@@ -59,9 +59,6 @@ func main() {
 				fmt.Printf("  :ORGANIZER: %s\n", event[0].GetOrganizer())
 				if event[0].GetGeo() != nil {
 					fmt.Printf("  :GEO: %v, \n", event[0].GetGeo())
-				}
-				if event[0].GetLocation() != "" {
-					fmt.Printf("  :LOCATION: %s \n", event[0].GetLocation())
 				}
 				tzids := ""
 				for _, tz := range event[0].GetDTZID() {
@@ -74,6 +71,9 @@ func main() {
 				fmt.Println("** Description\n")
 				for _, line := range strings.Split(event[0].GetDescription(), `\n`) {
 					fmt.Printf("  %s\n", strings.Replace(line, `\,`, ",", -1)) //remove escape from commas (a CSV thing)
+				}
+				if event[0].GetLocation() != "" {
+					fmt.Printf("** Location %s \n", event[0].GetLocation())
 				}
 			}
 		}
